6045a1c1551bcad02bbfa3996fb76c82ab888815
philv <philv@10.80.34.143>
2018-11-05 18:58:12 -0500

    IContactInformationFormatter and ContactInformationFormatter created

---
+++ src/edu/wmich/cs1120/LA4/Pvarner/Ccaudle/IContactInformationFormatter.java
@@ -0,0 +1,15 @@
+package edu.wmich.cs1120.LA4.Pvarner.Ccaudle;
+
+public interface IContactInformationFormatter {
+	void readContactInformation(String[] filePaths);
+	/*read contact information from a list of files given the file paths*/
+	void formatContactInformation(String fileName);
+	/*format the contact information in one file*/
+	void formatEmail(String email) throws EmailAddressFormatException;
+	/*format one email address*/
+	void formatPhoneNumber(String phoneNumber) throws PhoneNumberFormatException; /*Format one phone number*/
+	void formatName(String name) throws NameFormatException;
+	/*format one name*/
+
+
+}
